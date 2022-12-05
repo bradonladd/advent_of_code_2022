46c9bd4e2582b7c4d16c3eec49745e12030f1258
Bradon Ladd <bradon.laddy0614@gmail.com>
2022-12-05 10:45:36 -0800

    Uploading question 1

---
+++ 01_calorie_counting/part_01/main.dart
@@ -0,0 +1,26 @@
+// Code written by Bradon Ladd
+import 'dart:io';
+
+void main() {
+  new File('input.txt').readAsLines().then((List<String> content) {
+    var elves = [];
+    var elf = [];
+    for (var line in content) {
+      if (line == '') {
+        var total = 0;
+        for (var bag in elf) {
+          total = total + int.parse(bag);
+        }
+        elves.add(total);
+        elf.clear();
+      } else {
+        elf.add(line);
+      }
+    }
+
+    elves.sort();
+
+    print(elves.last);
+  });
+
+}
---
+++ 01_calorie_counting/part_02/main.dart
@@ -0,0 +1,27 @@
+// Code written by Bradon Ladd
+import 'dart:io';
+
+void main() {
+  new File('input.txt').readAsLines().then((List<String> content) {
+    var elves = [];
+    var elf = [];
+    for (var line in content) {
+      if (line == '') {
+        var total = 0;
+        for (var bag in elf) {
+          total = total + int.parse(bag);
+        }
+        elves.add(total);
+        elf.clear();
+      } else {
+        elf.add(line);
+      }
+    }
+
+    elves.sort();
+
+    // Grabbing the last three and adding them together via weird indexing.
+    print(elves.last + elves[elves.length - 2] + elves[elves.length - 3]);
+  });
+
+}
