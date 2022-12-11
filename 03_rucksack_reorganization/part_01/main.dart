11f8c506b2c8e8bda7bb22e640a7c620331dfe7e
Bradon Ladd <bradon.laddy0614@gmail.com>
2022-12-10 16:03:17 -0800

    P301 right answer

--- 03_rucksack_reorganization/part_01/main.dart
+++ 03_rucksack_reorganization/part_01/main.dart
@@ -1,30 +1,32 @@
 import 'dart:io';
 
 void main() {
-  double points = 0;
+  int points = 0;
 
   new File('input.txt').readAsLines().then((List<String> rucksacks) {
     
-    // print("ASCII Code for ${rucksacks[5][0]} is ${(rucksacks[5]).codeUnitAt(0)}");
-    // print("ASCII Code for ${rucksacks[1][0]} is ${(rucksacks[1]).codeUnitAt(0)}");
 
     for (var sack in rucksacks) {
-      // print('Evaluating sack $sack');
+
       Set duplicateItems = new Set();
       for (var item = 0; item < (sack.length / 2).floor(); item++) {
         duplicateItems.add(sack[item]);
       }
       // print("|------- Duplicate items contains: $duplicateItems");
 
-      for (int item = (sack.length / 2).floor() + 1;
+      for (int item = (sack.length / 2).floor();
           item < sack.length;
           item++) {
         if (duplicateItems.contains(sack[item])) {
-          points = points + ((sack.codeUnitAt(item) - 64) * 2);
+          if (sack.codeUnitAt(item) < 97) {
+            points = points + (sack.codeUnitAt(item) - 38);
+          } else {
+            points = points + (sack.codeUnitAt(item) - 96);
+          }
+          break;
         }
       }
     }
-
     print(points);
   });
 }
