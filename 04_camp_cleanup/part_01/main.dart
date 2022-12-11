25777fc075bb13f8aaad8b66b874fc3f502c162e
Bradon Ladd <bradon.laddy0614@gmail.com>
2022-12-11 03:08:42 -0800

    P401 and 02, mild refactoring

---
+++ 04_camp_cleanup/part_01/main.dart
@@ -0,0 +1,22 @@
+
+
+import 'dart:io';
+
+void main() {
+
+  new File('input.txt').readAsLines().then((List<String> sectionList) {
+    int counter = 0;
+
+    for (var sections in sectionList) {
+      List<String> sectionNums = sections.split(new RegExp("[-,]"));
+
+      if (int.parse(sectionNums[0]) <= int.parse(sectionNums[2]) && int.parse(sectionNums[1]) >= int.parse(sectionNums[3])) {
+        counter++;
+      } else if (int.parse(sectionNums[0]) >= int.parse(sectionNums[2]) && int.parse(sectionNums[1]) <= int.parse(sectionNums[3])) {
+        counter++;
+      }
+    }
+
+    print(counter);
+  });
+}
---
+++ 04_camp_cleanup/part_02/main.dart
@@ -0,0 +1,23 @@
+
+
+import 'dart:io';
+
+void main() {
+
+  new File('input.txt').readAsLines().then((List<String> sectionList) {
+    int counter = 0;
+
+    for (var sections in sectionList) {
+      List<String> sectionSplits = sections.split(new RegExp("[-,]"));
+      List<int> sectionNums = [int.parse(sectionSplits[0]), int.parse(sectionSplits[1]), int.parse(sectionSplits[2]), int.parse(sectionSplits[3])];
+
+      // If any section start/end is contained within the range of the other section range, the ranges overlap
+      if (sectionNums[0] >= sectionNums[2] && sectionNums[0] <= sectionNums[3]) { counter++; }
+      else if (sectionNums[1] >= sectionNums[2] && sectionNums[1] <= sectionNums[3]) { counter++; }
+      else if (sectionNums[2] >= sectionNums[0] && sectionNums[2] <= sectionNums[1]) { counter++; }
+      else if (sectionNums[3] >= sectionNums[0] && sectionNums[3] <= sectionNums[1]) { counter++; }
+    }
+
+    print(counter);
+  });
+}
