c5b56250adda211a084b45849f795b1777cfbcd7
Bradon Ladd <bradon.laddy0614@gmail.com>
2022-12-11 01:04:21 -0800

    P302

---
+++ 03_rucksack_reorganization/part_02/main.dart
@@ -0,0 +1,42 @@
+
+
+import 'dart:io';
+
+void main() {
+  int points = 0;
+
+  new File('input.txt').readAsLines().then((List<String> rucksacks) {
+    
+    for (int group = 0; group < rucksacks.length; group = group + 3) {
+      Set<String> firstElf = new Set();
+      Set<String> secondElf = new Set();
+      Set<String> thirdElf = new Set();
+
+      String firstSack = rucksacks[group];
+      String secondSack = rucksacks[group + 1];
+      String thirdSack = rucksacks[group + 2];
+
+      for (var item = 0; item < firstSack.length; item++) {
+        firstElf.add(firstSack[item]);
+      }
+      for (var item = 0; item < secondSack.length; item++) {
+        secondElf.add(secondSack[item]);
+      }
+      for (var item = 0; item < thirdSack.length; item++) {
+        thirdElf.add(thirdSack[item]);
+      }
+
+      for (var item in firstElf) {
+        if (secondElf.contains(item) && thirdElf.contains(item)) {
+          if (item.codeUnitAt(0) < 97) {
+            points = points + (item.codeUnitAt(0) - 38);
+          } else {
+            points = points + (item.codeUnitAt(0) - 96);
+          }
+        }
+      }
+    }
+
+    print(points);
+  });
+}
